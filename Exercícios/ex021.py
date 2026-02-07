137ee86abb35a2a9871346741ccf22c03440eeb1
Claudio Henrique <119673038+chrebelo@users.noreply.github.com>
2026-02-07 19:46:00 -0300

    Update ex021.py

--- Exercícios/ex021.py
+++ Exercícios/ex021.py
@@ -3,4 +3,5 @@ pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load('ex021.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
