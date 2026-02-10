9822a12bcc06bf396107ebe866c82a6a62ff1c1b
Claudio Henrique <119673038+chrebelo@users.noreply.github.com>
2026-02-09 22:38:45 -0300

    ex028

--- Exercícios/ex027.py
+++ Exercícios/ex027.py
@@ -4,4 +4,3 @@ print('Muito prazer em te conhecer!')
 print('Seu primeiro nome é {}'.format(nome[0]))
 # print('Seu último nome é {}'.format(nome[3]))
 print('Seu último nome é {}'.format(nome[len(nome)-1]))
-
